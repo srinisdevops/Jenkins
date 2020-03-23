ffa54708ffaa7d12bf93c6962767a2d99cf9f1eb
srinis <srinisdevops@gmail.com>
2020-03-23 21:22:10 +0000

    add files 02 03 folders

---
+++ 02/main.tf
@@ -0,0 +1,39 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "mybucket02222020"
+
+    #key    = "tfstatefiles/terraform.tfstate"
+    key    = "jenkins/02/terraform.tfstate"
+    region = "us-east-2"
+  }
+}
+
+resource "aws_instance" "srini_servers" {
+  ami = "ami-0fc20dd1da406780b"
+  #ami                    = "lookup(var.amiid, var.region)"
+  count                  = var.instance_count
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [var.security_group]
+  key_name               = var.key_name
+  root_block_device {
+    volume_size = 12
+  }
+  tags = {
+    #Name = "tf-example"
+    Name = "terraformInst--${count.index + 1}"
+  }
+  user_data = file("user_data.txt")
+}
+
+output "public_ip" {
+  value = aws_instance.srini_servers.*.public_ip
+}
+
+output "user_data" {
+  value = aws_instance.srini_servers.*.user_data
+}
+
---
+++ 02/user_data.sh
@@ -0,0 +1,13 @@
+
+#! /bin/bash
+# Jenkins install instructions for Ubuntu:
+
+
+sudo apt-get update -y
+sudo git clone https://github.com/munireddy/setupInstructions.git
+sudo chmod 777 ~/setupInstructions/Jenkins/jenkins.sh
+sudo bash ~/setupInstructions/Jenkins/jenkins.sh
+
+
+
+
